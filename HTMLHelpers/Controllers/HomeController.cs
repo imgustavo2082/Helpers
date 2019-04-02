440c88ec0960e699efb9c1ff6f408f2b4e36c0b5
gustavo montaño <elbeibleires@hotmail.com>
2019-04-02 00:14:20 -0400

    Helpers

---
+++ HTMLHelpers/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+          
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Persona persona)
+        {
+            ViewBag.Message = "Exitoso";
+            return View(persona);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+    
+    public class Persona
+    {
+        public int Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public int Telefono { get; set; }
+       
+        public string Correo { get; set; }
+        public string Genero { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Pasatiempo { get; set; }
+
+    }
+}
